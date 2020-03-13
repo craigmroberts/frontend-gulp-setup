9b4b8934394810167a69b702d36e3f4f632a29b9
Craig M Roberts <hello@craigmroberts.com>
2020-03-13 11:29:50 +0000

    Added styles functionality

---
+++ gulp/tasks/style.js
@@ -0,0 +1,32 @@
+'use strict';
+const
+    config = require("../config.json"),
+    pkg = require("../../package.json"),
+    gulp = require("gulp");
+
+// load all plugins in "devDependencies" into the variable $
+const $ = require("gulp-load-plugins")({
+    pattern: ["*"],
+    scope: ["devDependencies"]
+});
+
+// TASK: STYLES
+gulp.task('styles', function () {
+
+    return $.watch(config.styles.src, function () {
+
+        gulp.src(config.styles.src)
+        .pipe($.sourcemaps.init())
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe($.sass({ outputStyle: 'compressed' }).on('error', $.sass.logError)) // COMPRESS STYLES
+        .pipe($.autoprefixer())
+        .pipe($.rename(config.styles.dist.filename))
+        .pipe($.replace('"{{', '{{')) // SHOPIFY LIQUID
+        .pipe($.replace('}}"', '}}')) // SHOPIFY LIQUID
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest(config.styles.dist.directory));
+    });
+});
+
+
+
